94c9cc6e27ec1718ac4d6576e4042484385acdf6
UnRealDinnerbone <unrealdinnerbone@gmail.com>
2024-09-10 17:13:52 -0500

    1.21.1

--- neo/src/main/java/com/unrealdinnerbone/javd/JAVDNeo.java
+++ neo/src/main/java/com/unrealdinnerbone/javd/JAVDNeo.java
@@ -1,12 +1,15 @@
 package com.unrealdinnerbone.javd;
 
+import net.minecraft.world.entity.ai.attributes.Attributes;
+import net.minecraft.world.entity.player.Player;
 import net.neoforged.fml.common.Mod;
 
 @Mod(JAVD.MOD_ID)
 public class JAVDNeo {
     
     public JAVDNeo() {
-
+        Player player;
+        player.getAttribute(Attributes.ENTITY_INTERACTION_RANGE).setBaseValue(20.0D);
     }
 
 }
